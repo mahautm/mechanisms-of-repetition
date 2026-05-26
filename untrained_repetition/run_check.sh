b00d48038b639126763a79d6c104f797eb64b0a4
mahautm <mateo.mahaut@gmail.com>
2026-05-26 17:12:42 +0200

    Trim main to paper code

--- untrained_repetition/run_check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run empirical check of untrained models repetition
-
-srun --partition=alien --qos=alien --exclude=node044 bash -lc 'source ~/.bashrc && conda activate parr && python check_untrained.py'
